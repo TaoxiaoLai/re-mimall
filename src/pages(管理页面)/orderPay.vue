82a16d72626e2cfda6a05ddb3d362c55fa4119d8
laijintao <18811526955@163.com>
2020-04-10 20:54:41 +0800

    项目架构及路由封装

---
+++ src/pages(管理页面)/orderPay.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>order-pay</div>
+</template>
+<script>
+export default {
+  name: 'order-pay'
+}
+</script>
---
+++ src/router.js
@@ -0,0 +1,64 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './pages(管理页面)/home'
+import Index from './pages(管理页面)/index'
+import Product from './pages(管理页面)/product'
+import Detail from './pages(管理页面)/detail'
+import Order from './pages(管理页面)/order'
+import OrderList from './pages(管理页面)/orderList'
+import OrderConfirm from './pages(管理页面)/orderConfirm'
+import OrderPay from './pages(管理页面)/orderPay'
+import Cart from './pages(管理页面)/cart'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'index',
+          component: Index,
+        },{
+          path: '/product/:id',
+          name: 'product',
+          component: Product,
+        },{
+          path: '/detail/:id',
+          name: 'detail',
+          component: Detail,
+        }
+      ]
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: Order,
+      children: [
+        {
+          path: 'list',
+          name: 'order-list',
+          component: OrderList,
+        },{
+          path: 'confirm',
+          name: 'order-confirm',
+          component: OrderConfirm,
+        },{
+          path: 'pay',
+          name: 'order-pay',
+          component: OrderPay,
+        }
+      ]
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: Cart
+    }
+  ]
+})
