da4c4277595ffc678f981b6afa3ce538c89b42dc
laijintao <18811526955@163.com>
2020-05-09 11:29:52 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios,axios)
+
+// 接口错误拦截
+axios.interceptors.request.use((response) => {
+  let res = response.data
+  if(res.status == 0) {
+    return res.data
+  }else if(res.status == 10) {
+    window.location.href('#/login')
+  }else {
+    alert(res.msg)
+  }
+})
 
 new Vue({
   router,
--- src/storage(数据存储工具箱)/index.js
+++ src/storage(数据存储工具箱)/index.js
@@ -4,10 +4,13 @@
 
 const STORAGE_KEY = 'mall'
 export default{
+  // 获取整个sessionStorage，并转化为json形式
   getStorage() {
     return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}')
   },
+  // 获取某个值
   getItem(key, module_name) {
+    // 比如获取user下面的age属性
     if(module_name) {
       // let val = this.getStorage()[module_name]
       let val = this.getItem(module_name)
@@ -15,6 +18,7 @@ export default{
     }
     return this.getStorage()[key]
   },
+  // 设置值
   setItem(key, value, module_name) {
     if(module_name) {
       let val = this.getItem(module_name)
@@ -26,6 +30,7 @@ export default{
       return window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(stor))
     }
   },
+  // 删除值
   clear(key, module_name) {
     let val = this.getStorage()
     if(module_name) {
