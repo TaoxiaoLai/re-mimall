4c25fe8a02da8e8121068486228249f6c6fd79f6
laijintao <18811526955@163.com>
2020-05-14 20:44:26 +0800

    手机商品栏的实现

--- src/pages(管理页面)/index.vue
+++ src/pages(管理页面)/index.vue
@@ -50,9 +50,41 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
-      <div class="product-box"></div>
+      <div class="ads-box">
+        <a :href="'/#/product/' + item.id" v-for="(item, index) in adsList" :key="index">
+          <img :src="item.img">
+        </a>
+      </div>
+      <div class="banner">
+        <a href="/#/product/30">
+          <img src="/imgs/banner-1.png">
+        </a>
+      </div>
+    </div>
+    <div class="product-box">
+      <div class="container">
+        <h2>手机</h2>
+        <div class="wrapper">
+          <div class="banner-left">
+            <a href="/#/product/35"><img src="/imgs/mix-alpha.jpg" alt=""></a>
+          </div>
+          <div class="list-box">
+            <div class="list" v-for="(arr, index) in phoneList" :key="index">
+              <div class="item" v-for="(item, j) in arr" :key="item.id">
+                <span :class="{'new-pro': j%2 == 0}">新品</span>
+                <div class="item-img">
+                  <img :src="item.mainImage">
+                </div>
+                <div class="item-info">
+                  <h3>{{item.name}}</h3>
+                  <p>{{item.subtitle}}</p>
+                  <p class="price">{{item.price}}元</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <service-bar></service-bar>
   </div>
@@ -92,55 +124,87 @@ export default {
         },
       },
       slideList:[
+        {
+          id:'42',
+          img:'/imgs/slider/slide-1.jpg'
+        },
+        {
+          id:'45',
+          img:'/imgs/slider/slide-2.jpg'
+        },
+        {
+          id:'46',
+          img:'/imgs/slider/slide-3.jpg'
+        },
+        {
+          id:'',
+          img:'/imgs/slider/slide-4.jpg'
+        },
+        {
+          id:'',
+          img:'/imgs/slider/slide-1.jpg'
+        }
+      ],
+      menuList:[
+        [
           {
-            id:'42',
-            img:'/imgs/slider/slide-1.jpg'
-          },
-          {
-            id:'45',
-            img:'/imgs/slider/slide-2.jpg'
-          },
-          {
-            id:'46',
-            img:'/imgs/slider/slide-3.jpg'
-          },
-          {
-            id:'',
-            img:'/imgs/slider/slide-4.jpg'
-          },
-          {
-            id:'',
-            img:'/imgs/slider/slide-1.jpg'
+            id:30,
+            img:'/imgs/item-box-1.png',
+            name:'小米CC9',
+          },{
+            id:31,
+            img:'/imgs/item-box-2.png',
+            name:'小米8青春版',
+          },{
+            id:32,
+            img:'/imgs/item-box-3.jpg',
+            name:'Redmi K20 Pro',
+          },{
+            id:33,
+            img:'/imgs/item-box-4.jpg',
+            name:'移动4G专区',
           }
         ],
-        menuList:[
-          [
-            {
-              id:30,
-              img:'/imgs/item-box-1.png',
-              name:'小米CC9',
-            },{
-              id:31,
-              img:'/imgs/item-box-2.png',
-              name:'小米8青春版',
-            },{
-              id:32,
-              img:'/imgs/item-box-3.jpg',
-              name:'Redmi K20 Pro',
-            },{
-              id:33,
-              img:'/imgs/item-box-4.jpg',
-              name:'移动4G专区',
-            }
-          ],
-          [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
-        ],
+        [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+      ],
+      adsList:[
+        {
+          id:33,
+          img:'/imgs/ads/ads-1.png'
+        },{
+          id:48,
+          img:'/imgs/ads/ads-2.jpg'
+        },{
+          id:45,
+          img:'/imgs/ads/ads-3.png'
+        },{
+          id:47,
+          img:'/imgs/ads/ads-4.jpg'
+        }
+      ],
+      phoneList: [],
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.axios.get('/products', {
+        params: {
+          categoryId: 100012,
+          pageSize: 14
+        }
+      }).then((res) => {
+        this.phoneList = [res.list.slice(6,10), res.list.slice(10,14)]
+      })
     }
   }
 }
 </script>
 
 <style lang="scss">
+  @import './../assets/scss/base.scss';
   @import './../assets/scss/config.scss';
   @import './../assets/scss/mixin.scss';
   .index {
@@ -219,6 +283,103 @@ export default {
         img {
           width: 100%;
           height: 100%;
+        }
+      }
+    }
+    .ads-box {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 14px;
+      margin-bottom: 31px;
+      a {
+        width: 296px;
+        height: 167px;
+      }
+    }
+    .banner-box {
+      margin-bottom: 50px;
+    }
+    .product-box {
+      background-color: $colorJ;
+      padding: 30px 0 50px;
+      h2 {
+        font-size: $fontF;
+        height: 21px;
+        line-height: 21px;
+        color: $colorB;
+        margin-bottom: 20px;
+      }
+      .wrapper {
+        display: flex;
+        .banner-left {
+          margin-right: 16px;
+          img {
+            width: 224px;
+            height: 619px;
+          }
+        }
+        .list-box {
+          .list {
+            display: flex;
+            justify-content: space-between;
+            width: 986px;
+            margin-bottom: 14px;
+            &:last-child {
+              margin-bottom: 0;
+            }
+            .item {
+              width: 236px;
+              height: 302px;
+              background-color: $colorG;
+              text-align: center;
+              span {
+                display: inline-block;
+                width: 67px;
+                height: 24px;
+                font-size: 14px;
+                line-height: 24px;
+                color: #ffffff;
+                &.new-pro{
+                  background-color:#7ECF68;
+                }
+                &.kill-pro{
+                  background-color:#E82626;
+                }
+              }
+              .item-img{
+                img{
+                  width:100%;
+                  height:195px;
+                }
+              }
+              .item-info {
+                h3{
+                  font-size:$fontJ;
+                  color:$colorB;
+                  line-height:$fontJ;
+                  font-weight:bold;
+                }
+                p{
+                  color:$colorD;
+                  line-height:13px;
+                  margin:6px auto 13px;
+                }
+                .price {
+                  color: #f20a0a;
+                  font-size: $fontJ;
+                  font-weight: bold;
+                  cursor: pointer;
+                  &::after {
+                    display: inline-block;
+                    content: ' ';
+                    @include bgImg(22px, 22px, '/imgs/icon-cart-hover.png');
+                    vertical-align: middle;
+                    margin-left: 5px;
+                  }
+                }
+              }
+            }
+          }
         }
       }
     }
