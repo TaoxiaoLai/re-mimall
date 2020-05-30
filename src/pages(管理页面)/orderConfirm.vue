e9ef345b5a405ae56f77c584b562bd0c9671f699
laijintao <18811526955@163.com>
2020-05-30 15:39:38 +0800

    订单页面完全实现

--- src/pages(管理页面)/orderConfirm.vue
+++ src/pages(管理页面)/orderConfirm.vue
@@ -5,22 +5,45 @@
         <span>请认真填写收货地址</span>
       </template>
     </order-header>
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="position: absolute; width: 0px; height: 0px; overflow: hidden;">
+      <defs>
+        <symbol id="icon-add" viewBox="0 0 31 32">
+          <title>add</title>
+          <path d="M30.745 15.152h-14.382v-14.596c0-0.308-0.243-0.557-0.543-0.557s-0.543 0.249-0.543 0.557v14.596h-14.665c-0.3 0-0.543 0.249-0.543 0.557s0.243 0.557 0.543 0.557h14.665v15.177c0 0.307 0.243 0.557 0.543 0.557s0.543-0.249 0.543-0.557v-15.177h14.382c0.3 0 0.543-0.249 0.543-0.557s-0.243-0.557-0.543-0.557z" class="path1"></path>
+        </symbol>
+        <symbol id="icon-edit" viewBox="0 0 32 32">
+          <title>edit</title>
+          <path d="M28.287 8.51l-4.805-4.806 0.831-0.831c0.472-0.472 1.086-0.777 1.564-0.777 0.248 0 0.452 0.082 0.622 0.253l3.143 3.144c0.539 0.54 0.133 1.529-0.524 2.186l-0.831 0.831zM26.805 9.992l-1.138 1.138-4.805-4.806 1.138-1.138 4.805 4.806zM24.186 12.612l-14.758 14.762-4.805-4.806 14.758-14.762 4.805 4.806zM7.379 28.288l-4.892 1.224 1.223-4.894 3.669 3.67zM31.123 4.011l-3.143-3.144c-0.567-0.567-1.294-0.867-2.103-0.867-1.036 0-2.174 0.52-3.045 1.391l-20.429 20.436c-0.135 0.134-0.23 0.302-0.276 0.487l-2.095 8.385c-0.089 0.355 0.017 0.736 0.276 0.995 0.198 0.198 0.461 0.307 0.741 0.307 0.085 0 0.171-0.010 0.254-0.031l8.381-2.096c0.185-0.047 0.354-0.142 0.487-0.276l20.43-20.436c1.409-1.41 2.042-3.632 0.524-5.15v0z" class="path1"></path>
+        </symbol>
+        <symbol id="icon-del" viewBox="0 0 32 32">
+          <title>delete</title>
+          <path d="M11.355 4.129v-2.065h9.29v2.065h-9.29zM6.194 29.935v-23.742h19.613v23.742h-19.613zM30.968 4.129h-8.258v-3.097c0-0.569-0.463-1.032-1.032-1.032h-11.355c-0.569 0-1.032 0.463-1.032 1.032v3.097h-8.258c-0.569 0-1.032 0.463-1.032 1.032s0.463 1.032 1.032 1.032h3.097v24.774c0 0.569 0.463 1.032 1.032 1.032h21.677c0.569 0 1.032-0.463 1.032-1.032v-24.774h3.097c0.569 0 1.032-0.463 1.032-1.032s-0.463-1.032-1.032-1.032v0z" class="path1"></path>
+          <path d="M10.323 9.806c-0.569 0-1.032 0.463-1.032 1.032v14.452c0 0.569 0.463 1.032 1.032 1.032s1.032-0.463 1.032-1.032v-14.452c0-0.569-0.463-1.032-1.032-1.032z" class="path2"></path>
+          <path d="M16 9.806c-0.569 0-1.032 0.463-1.032 1.032v14.452c0 0.569 0.463 1.032 1.032 1.032s1.032-0.463 1.032-1.032v-14.452c0-0.569-0.463-1.032-1.032-1.032z" class="path3"></path>
+          <path d="M21.677 9.806c-0.569 0-1.032 0.463-1.032 1.032v14.452c0 0.569 0.463 1.032 1.032 1.032s1.032-0.463 1.032-1.032v-14.452c0-0.569-0.463-1.032-1.032-1.032z" class="path4"></path>
+        </symbol>
+      </defs>
+    </svg>
     <div class="wrapper">
       <div class="container">
         <div class="order-box">
           <div class="item-address">
             <h2 class="addr-title">收货地址</h2>
             <div class="addr-list clearfix">
-              <div class="addr-info" v-for="(item, index) in list" :key="index">
+              <div class="addr-info" :class="{'checked':index == checkIndex}" @click="checkIndex = index" v-for="(item, index) in list" :key="index">
                 <h2>{{item.receiverName}}</h2>
                 <div class="phone">{{item.receiverMobile}}</div>
                 <div class="street">{{item.receiverProvince + ' ' + item.receiverCity + ' ' + item.receiverDistrict + ' ' + item.receiverAddress}}</div>
                 <div class="action">
-                  <a href="javascript:;" class="fl">
-                    <img src="" alt="" style="display:inline-block; width:12px; height:12px; background:yellow;">
+                  <a href="javascript:;" class="fl" @click="openDelModal(item)">
+                    <svg class="icon icon-del">
+                      <use xlink:href="#icon-del"></use>
+                    </svg>
                   </a>
-                  <a href="javascript:;" class="fr">
-                    <img src="" alt="" style="display:inline-block; width:12px; height:12px; background:yellow;">
+                  <a href="javascript:;" class="fr" @click="openEditModal(item)">
+                    <svg class="icon icon-del">
+                      <use xlink:href="#icon-edit"></use>
+                    </svg>
                   </a>
                 </div>
               </div>
@@ -76,27 +99,87 @@
           </div>
           <div class="btn-group">
             <a href="/#/cart" class="btn btn-default btn-large">返回购物车</a>
-            <a href="javascript:;" class="btn btn-large">去结算</a>
+            <a href="javascript:;" class="btn btn-large" @click="orderConfirm">去结算</a>
           </div>
         </div>
       </div>
     </div>
+    <modal
+      title="删除确认"
+      btnType="1"
+      :showModal="showDelModal"
+      @cancle="showDelModal = false"
+      @submit="submitAddress"
+    >
+      <template v-slot:body>
+        <span>您确认要删除此地址吗？</span>
+      </template>
+    </modal>
+    <modal
+      title="新增确认"
+      btnType="1"
+      :showModal="showEditModal"
+      @cancle="showEditModal = false"
+      @submit="submitAddress"
+    >
+      <template v-slot:body>
+        <div class="edit-wrap">
+          <div class="item">
+            <input type="text" class="input" placeholder="姓名" v-model="checkedItem.receiverName">
+            <input type="text" class="input" placeholder="手机号" v-model="checkedItem.receiverMobile">
+          </div>
+          <div class="item">
+            <select name="province" v-model="checkedItem.receiverProvince">
+              <option value="北京">北京</option>
+              <option value="天津">天津</option>
+              <option value="河北">河北</option>
+            </select>
+            <select name="city" v-model="checkedItem.receiverCity">
+              <option value="北京">北京</option>
+              <option value="天津">天津</option>
+              <option value="河北">石家庄</option>
+            </select>
+            <select name="district" v-model="checkedItem.receiverDistrict">
+              <option value="北京">昌平区</option>
+              <option value="天津">海淀区</option>
+              <option value="河北">东城区</option>
+              <option value="天津">西城区</option>
+              <option value="河北">顺义区</option>
+              <option value="天津">房山区</option>
+            </select>
+          </div>
+          <div class="item">
+            <textarea name="street" v-model="checkedItem.receiverAddress"></textarea>
+          </div>
+          <div class="item">
+            <input type="text" class="input" placeholder="邮编" v-model="checkedItem.receiverZip">
+          </div>
+        </div>
+      </template>
+    </modal>
   </div>
 </template>
 
 <script>
 import OrderHeader from './../components/OrderHeader'
+import Modal from './../components/Modal'
 export default {
   name: 'order-confirm',
   components: {
-    OrderHeader
+    OrderHeader,
+    Modal
   },
   data() {
     return {
       list: [], //收货地址
       cartList: [], //选中的商品列表
       count: 0, //商品结算数量
-      totalPrice: 0 //商品总价格
+      totalPrice: 0, //商品总价格
+      checkedItem: {}, //选中的商品对象
+      userAction: '',  //用户行为 0:新增，1:编辑，2:删除
+      showDelModal: false, //删除弹框显示控制
+      showEditModal: false, //新增和编辑地址弹框控制
+      checkIndex: 0 //当前收货地址选中索引
     }
   },
   mounted() {
@@ -109,6 +192,75 @@ export default {
         this.list = res.list
       })
     },
+    openDelModal(item) {
+      this.checkedItem = item
+      this.userAction = 2
+      this.showDelModal = true
+    },
+    openEditModal(item) {
+      this.checkedItem = item
+      this.userAction = 1
+      this.showEditModal = true
+    },
+    openAddressModal() {
+      this.checkedItem = {}
+      this.userAction = 0
+      this.showEditModal = true
+    },
+    submitAddress() {
+      let url,method,params = {}
+      let {userAction, checkedItem} = this
+      if(userAction == 0) {
+        url = '/shippings', method = 'post'
+      }else if(userAction == 1) {
+        url = `/shippings/${checkedItem.id}`, method = 'put'
+      }else {
+        url = `/shippings/${checkedItem.id}`, method = 'delete'
+      }
+      if(userAction == 0 || userAction == 1) {
+        let { receiverName, receiverMobile, receiverProvince, receiverCity, receiverDistrict, receiverAddress, receiverZip} = checkedItem;
+        let errMsg = ''
+        if(!receiverName) {
+          errMsg = '请输入正确的用户名'
+        }else if(!receiverMobile || !/\d{11}/.test(receiverMobile)) {
+          errMsg = '请输入正确的手机号码'
+        }else if(!receiverProvince) {
+          errMsg = '请输入省份'
+        }else if(!receiverCity) {
+          errMsg = '请输入城市'
+        }else if(!receiverDistrict) {
+          errMsg = '请输入街道'
+        }else if(!receiverAddress) {
+          errMsg = '请输入详细地址'
+        }else if(!/\d{6}/.test(receiverZip)) {
+          errMsg = '请输入6位邮编'
+        }
+        if(errMsg) {
+          this.$message.error(errMsg)
+          return
+        }
+        params = {
+          receiverName,
+          receiverMobile,
+          receiverProvince,
+          receiverCity,
+          receiverDistrict,
+          receiverAddress,
+          receiverZip
+        }
+      }
+      this.axios[method](url, params).then(() => {
+        this.closeModal()
+        this.getAddressList()
+        this.$message.success('操作成功')
+      })
+    },
+    closeModal() {
+      this.showDelModal = false
+      this.showEditModal = false
+      this.checkedItem = {}
+      this.userAction = ''
+    },
     getCartList() {
       this.axios.get('/carts').then((res) => {
         let list = res.cartProductVoList
@@ -119,8 +271,22 @@ export default {
         })
       })
     },
-    openAddressModal() {
-
+    orderConfirm() {
+      let item = this.list[this.checkIndex]
+      if(!item) {
+        this.$message.error('请选择一个收货地址')
+        return
+      }
+      this.axios.post('/orders',{
+        shippingId: item.id
+      }).then((res) => {
+        this.$router.push({
+          path: '/order/pay',
+          query: {
+            orderNo: res.orderNo
+          }
+        })
+      })
     }
   }
 }
@@ -183,6 +349,21 @@ export default {
               }
               &.checked{
                 border:1px solid #ff6700;
+              }
+            }
+            .addr-add{
+              text-align:center;
+              color: #999999;
+              cursor:pointer;
+              .icon-add{
+                width:30px;
+                height:30px;
+                border-radius:50%;
+                background:url('/imgs/icon-add.png') #E0E0E0 no-repeat center;
+                background-size:14px;
+                margin: 0 auto;
+                margin-top: 45px;
+                margin-bottom: 10px;
               }
             }
           }
@@ -261,6 +442,37 @@ export default {
         .btn-group {
           margin-top: 37px;
           text-align: right;
+        }
+      }
+    }
+    .edit-wrap {
+      font-size: 14px;
+      .item {
+        margin-bottom: 15px;
+        input {
+          display: inline-block;
+          width: 293px;
+          height: 40px;
+          line-height: 40px;
+          padding-left: 15px;
+          box-sizing: border-box;
+          border: 1px solid #e5e5e5;
+          &+.input {    // 选中当前元素的兄弟元素
+            margin-left: 14px;  
+          }
+        }
+        select {
+          height: 40px;
+          line-height: 40px;
+          margin-right: 15px;
+          border: 1px solid #e5e5e5;
+        }
+        textarea {
+          height: 62px;
+          width: 100%;
+          padding: 13px 15px;
+          border: 1px solid #e5e5e5;
+          box-sizing: border-box;
         }
       }
     }
